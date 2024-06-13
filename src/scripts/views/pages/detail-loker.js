a4872d520ebcf94c14ee6981649d00548e91dfcb
Dwita Anatasya <160572652+dwitaanatasya@users.noreply.github.com>
2024-06-13 16:02:45 +0700

    Update detail-loker.js
    struktur data loker yang ditampilkan (data perlu dihubungkan dengan database)

--- src/scripts/views/pages/detail-loker.js
+++ src/scripts/views/pages/detail-loker.js
@@ -2,6 +2,23 @@ const Detail = {
   async render() {
       return `
       <div class="data-loker">
+            <h2>Posisi Pekerjaan</h2> <!-- Fetch Data -->
+            <h4>Nama Perusahaan, Lokasi</h4> <!-- Fetch Data -->
+                <div class="icon-sosmed"></div>
+            <p><strong>Gaji:</strong> <!-- Fetch Data --> </P>
+            <p><strong>Level Pekerjaan:</strong> <!-- Fetch Data --></p>
+            <p><strong>Pendidikan:</strong> <!-- Fetch Data --></p>
+            <p><strong>Tipe Pekerjaan:</strong> <!-- Fetch Data --></p>
+                <p>penjelasan tipe pekerjaan</p> <!-- Fetch Data -->
+            <p><strong>Tanggung Jawab Pekerjaan:</strong></P>
+                <p>list tanggung jawab pekerjaan</p> <!-- Fetch Data -->
+            <p><strong>Keahlian yang Dibutuhkan:</strong></P>
+                <p>list keahlian</p> <!-- Fetch Data -->
+            <p><strong>Kualifikasi:</strong></P>
+                <p>list kualifikasi</p> <!-- Fetch Data -->
+            <p><strong>Jam Kerja:</strong></P>
+                <p>misal 08.00 - 17.00</p><!-- Fetch Data -->
+            <h5> Tanggal Posting : (diambil dari database juga) </h5>
       </div>
     `;
   },
